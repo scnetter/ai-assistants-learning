207f706b42c55296616d1ec69c4bcce6af554ebd
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-04-30 16:40:10 -0500

    Few shots prompts created with reply history

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import os
 if __name__ == '__main__':
     client = OpenAI()
 
-    response = client.chat.completions.create(
-        model='gpt-3.5-turbo',
-        messages= [
-            {'role': 'system',
+    print('Welcome to ChatsRUs! How can I help you?')
+    messages = [{'role': 'system',
              'content': """You are a nerdy and humorous but friendly and helpful assistant. 
-             You are an expert python programmer."""
-             },
-            {'role': 'user',
-             'content': 'What is the capital of France.'}
-        ]
-    )
+             You are an expert python programmer. Don't forget to mention ChatsRUs!"""
+             }]
 
-    print(response.choices[0].message.content)
+    question = ""
+
+    while question != 'BYE':
+        question = input()
+        messages.append({'role': 'user', 'content': question})
+
+
+        response = client.chat.completions.create(
+            model='gpt-3.5-turbo',
+            messages= messages,
+        )
+        reply = response.choices[0].message.content
+        print(reply)
+        messages.append({'role': 'assistant', 'content': reply})
