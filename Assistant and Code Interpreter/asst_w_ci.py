f6bf43b5af80ae3881e2eaf36ad1ac41a326bf78
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-05-01 17:15:19 -0500

    Exploration of threads, runs, and assistants api methods

--- Assistant and Code Interpreter/asst_w_ci.py
+++ Assistant and Code Interpreter/asst_w_ci.py
@@ -18,3 +18,36 @@ message = client.beta.threads.messages.create(thread_id=thread.id,
                                               content='What is 123456 * 46789')
 
 # Take messages from user and choose an assistant to run the thread - can use specialized asst
+run = client.beta.threads.runs.create(thread_id=thread.id,assistant_id=assistant.id)
+# check the status of a run
+print(run.status)
+# retrieve the run to get the updated status until 'complete'
+run = client.beta.threads.runs.retrieve(run_id=run.id, thread_id=thread.id)
+print(run.status)
+# thread -> Assistant RUN -> add new message to thread
+# get messages from thread after complete
+messages = client.beta.threads.messages.list(thread_id=thread.id)
+# to get the most recent message
+print(messages.data[0].content[0].text.value)
+# to iterate through the messages
+for message_thread in messages:
+    print(message_thread.content[0].text.value)
+    print("\n")
+
+# to see the run steps:
+run_steps = client.beta.threads.runs.steps.list(thread_id=thread.id, run_id=run.id)
+
+for step in run_steps:
+    print(step.step_details)
+    print("\n")
+
+# to see your assistants
+assistants = client.beta.assistants.list(limit='20', order='desc')
+
+for assistant in assistants:
+    print(assistant.name)
+    print("\n")
+
+# to delete an assistant
+response = client.beta.assistants.delete(assistant_id=assistant.id)
+# errors usually mean assistant was already deleted or id is incorrect
